e80210345a4742d215439d7544574d31bb99d8ed
richardowsky <brooklyn.od.ua@gmail.com>
2020-04-22 20:41:04 +0300

    setup redis

---
+++ src/controllers/redis.go
@@ -0,0 +1,24 @@
+package controllers
+
+import (
+	"fmt"
+	"github.com/go-redis/redis"
+	"time"
+)
+
+var ClientForToken = NewClient(0)
+
+func NewClient(database int) *redis.Client {
+	client := redis.NewClient(&redis.Options{
+		Addr:            "localhost:6379",
+		Password:        "",       // no password set
+		DB:              database, // use default DB
+		MaxRetries:      5,
+		MinRetryBackoff: time.Second,
+		MaxRetryBackoff: 5 * time.Second,
+	})
+
+	pong, err := client.Ping().Result()
+	fmt.Println(pong, err)
+	return client
+}
